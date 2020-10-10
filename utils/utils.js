5bd0924ced7d390fd464eeeea80571bf411f853a
Bartosz Kalota <bartek.kalota@wp.pl>
2020-10-10 16:34:48 +0200

    Util getting data from mocks

--- utils/utils.js
+++ utils/utils.js
@@ -1,2 +1,11 @@
 import fs from 'fs';
 import path from 'path';
+
+export const getDataFromFile = async (fileName) => {
+  const filePath = path.resolve(`./mocks/${fileName}`);
+  try {
+    return JSON.parse(await fs.promises.readFile(filePath, 'utf-8'));
+  } catch (err) {
+    console.log(err);
+  }
+};
