ad63ebdee6cc4940c5f16c4361f00496fdea4e6d
Bartosz Kalota <bartek.kalota@wp.pl>
2020-10-10 16:42:32 +0200

    Router handling mocks

--- routes/weather-mocks.js
+++ routes/weather-mocks.js
@@ -2,3 +2,48 @@ import express from 'express';
 import { getDataFromFile } from '../utils/utils.js';
 
 export const router = express.Router();
+
+router.get('/weatherbit-current', async (req, res, next) => {
+  try {
+    const data = await getDataFromFile('weatherbit_current.json');
+    res.json(data);
+  } catch (err) {
+    next(err)
+  }
+});
+
+router.get('/openweathermap-current', async (req, res, next) => {
+  try {
+    const data = await getDataFromFile('openweathermap_current.json');
+    res.json(data);
+  } catch (err) {
+    next(err)
+  }
+});
+
+router.get('/accuweather-current', async (req, res, next) => {
+  try {
+    const data = await getDataFromFile('accuweather_current.json');
+    res.json(data);
+  } catch (err) {
+    next(err)
+  }
+});
+
+router.get('/accuweather-location', async (req, res, next) => {
+  try {
+    const data = await getDataFromFile('accuweather_location.json');
+    res.json(data);
+  } catch (err) {
+    next(err)
+  }
+});
+
+router.get('/accuweather-daily', async (req, res, next) => {
+  try {
+    const data = await getDataFromFile('accuweather_daily.json');
+    res.json(data);
+  } catch (err) {
+    next(err)
+  }
+});
