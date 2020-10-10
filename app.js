8894a6be039b80455c7ad6fca407fc0c9268bca7
Bartosz Kalota <bartek.kalota@wp.pl>
2020-10-10 16:35:44 +0200

    App errors handling middleware

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 import express from 'express';
-// import { router as mocks } from './routes/weather-mocks.js';
+import { router as mocks } from './routes/weather-mocks.js';
 // import { router as weather } from './routes/weather.js';
-import ENVS from './config.js'; 
+import ENVS from './config.js';
 
 const app = express();
+
 // app.use(express.static('weather'));
 
-// app.use('/mocks', mocks);
+app.use('/mocks', mocks);
 // app.use('/weather', weather);
+
+app.use((err, req, res, next) => {
+  res.send(err.message);
+});
 
 app.listen(ENVS.PORT, () => {
   console.log(`Listening on port ${ENVS.PORT}...`);
