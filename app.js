05839237c0a90ac6581090b8530c760399320015
Bartosz Kalota <bartek.kalota@wp.pl>
2020-10-12 13:55:43 +0200

    Front-end activation

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import { router as mocks } from './routes/weather-mocks.js';
-// import { router as weather } from './routes/weather.js';
+import { router as weather } from './routes/weather.js';
 import ENVS from './config.js';
 
 const app = express();
 
-// app.use(express.static('weather'));
+app.use(express.static('weather'));
 
 app.use('/mocks', mocks);
-// app.use('/weather', weather);
+app.use('/weather', weather);
 
 app.use((err, req, res, next) => {
   res.send(err.message);
